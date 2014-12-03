a70e0e8eb073e14439474e181a7b872f038560c7
CODERLETICS-SHAREE <SHAREEWASHCODER@GMAIL.COM>
2014-12-02 23:22:08 -0500

    bootstap added and customized

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def imprint
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  root "pages#home"
+  get "imprint"=> "pages#imprint"
+  get "contact"=> "pages#contact"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'sites#index'
+  # root 'sites#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
