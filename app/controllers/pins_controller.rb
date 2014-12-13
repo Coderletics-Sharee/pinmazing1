3e54e94efd906ddf44fa1db3ffa0013f7d40d53d
CODERLETICS-SHAREE <SHAREEWASHCODER@GMAIL.COM>
2014-12-13 09:33:04 -0500

    updated pins_controller page w/ placement of authentication script

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,8 @@
 class PinsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
 
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @pins = Pin.all
