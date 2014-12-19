67c6aab837b76a72a9dd7468d4b238da04c76267
CODERLETICS-SHAREE <SHAREEWASHCODER@GMAIL.COM>
2014-12-18 23:01:00 -0500

    modified comments controller and view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,11 +4,17 @@ class CommentsController < ApplicationController
   
   def create
     @pin = Pin.find(params[:pin_id])
-    @comment = @pin.comments.create(params[:comment])
-    if @comment.save
-    redirect_to @pin, notice: 'Comment was successfully created.'
-    else
-    render action: 'new'  
+    @comment = @pin.comments.create(params[:comment].permit(:body))
+    
+    respond_to do |format|	
+if @comment.save
+format.html { redirect_to @pin, notice: 'Comment was successfully created.'}
+format.json { render json: @comment, status: :created, location: @comment}
+else
+format.html { render action: "new" }
+format.json { render json: @comment.errors, status: :unprocessable_entity} 
 end
 end
 end
+end
+
