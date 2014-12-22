3c16b99614b25c1eef8317bee5da0278b35b58d9
CODERLETICS-SHAREE <SHAREEWASHCODER@GMAIL.COM>
2014-12-22 10:36:09 -0500

    Delete function works

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,47 +1,31 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
-
-before_action :authenticate_user!, except: [:index, :show]
-  
-  def edit
-  end
+  #before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
   def create
     @pin = Pin.find(params[:pin_id])
     @comment = @pin.comments.create(params[:comment].permit(:body))
     
-    respond_to do |format|  
-if @comment.save
-format.html { redirect_to @pin, notice: 'Comment was successfully created.'}
-format.json { render json: @comment, status: :created, location: @comment}
-else
-format.html { render action: "new" }
-format.json { render json: @comment.errors, status: :unprocessable_entity} 
-end
-end
-end
+    @comment.pin_id = @pin.id
+    @comment.user_id = current_user.id
 
-def update
-    respond_to do |format|
-      if @comment.update(comment_params)
-        format.html { redirect_to @pin, notice: 'Comment was successfully updated.' }
-        format.json { render json: @comment, status: :created, location: @comment}
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
+    respond_to do |format|  
+    if @comment.save
+    format.html { redirect_to @pin, notice: 'Comment was successfully created.'}
+    format.json { render json: @comment, status: :created, location: @comment}
+    else
+    format.html { render action: "new" }
+    format.json { render json: @comment.errors, status: :unprocessable_entity} 
     end
-  end
+    end
+end
   
   def destroy
-  @comment = @pin.comments.find(params[:id])
+    @pin = Pin.find(params[:pin_id])
+    @comment = @pin.comments.find(params[:id])
     @comment.destroy
-    respond_to do |format|
-      format.html { redirect_to comments_url }
-      format.json { head :no_content }
-    end
-  end 
-end
+    redirect_to @pin
+  end
+end 
 
     
     
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ActiveRecord::Base
-belongs_to :pin
+	belongs_to :pin
+	belongs_to :user
 end
