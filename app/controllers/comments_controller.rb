8ced1f38b2c95017f2ca57218b55c9f9eb8c0495
CODERLETICS-SHAREE <SHAREEWASHCODER@GMAIL.COM>
2015-01-06 14:23:00 -0500

    updated edit function- still doesn't work

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,50 +1,61 @@
 class CommentsController < ApplicationController
-  #before_action :set_comment, only: [:show, :edit, :update, :destroy]
-#def new
-  #@pin = Pin.find(params[:pin_id])
-  #@comment = current_user.comments.build
-#end
+  #before_filter :user_signed_in
+
+  def index
+    @comments = Comment.all
+  end
+
+  def new
+    @comment = @pin.comments.build(params[:comment].permit(:body))
+  end
 
   def create
     @pin = Pin.find(params[:pin_id])
-    @comment = @pin.comments.create(params[:comment].permit(:body))
-
+    @comment = @pin.comments.build(params[:comment].permit(:body))
     @comment.pin_id = @pin.id
     @comment.user_id = current_user.id
-
-    respond_to do |format|  
-    if @comment.save
-    format.html { redirect_to @pin, notice: 'Comment was successfully created.'}
-    format.json { render json: @comment, status: :created, location: @comment}
-    else
-    format.html { render action: "new" }
-    format.json { render json: @comment.errors, status: :unprocessable_entity} 
-    end
-    end
+    
+    respond_to do |format|
+  if @comment.save
+    format.html {redirect_to @pin, notice: "Comment was successfully created"}
+    format.json {render json: @pin, status: :created, location: @comment}
+  else
+    format.html {render action: 'new'}
+    format.json {render json: @comment.errors, status: :unprocessable_entity}
+  end
+  end
 end
+
+  def show
+    @pin = Pin.find(params[:pin_id])
+    @comment = @pin.comments.find(params[:id])
+  end
+
+  def edit
+    @pin = Pin.find(params[:pin_id])
+    @comment = @pin.comments.find(params[:id])
+  end 
 
   def update
     @pin = Pin.find(params[:pin_id])
     @comment = @pin.comments.find(params[:id])
+    @pin = @comment.pin
 
-    @comment.pin_id = @pin.id
-    @comment.user_id = current_user.id
-
-    if @comment.update
-        redirect_to @pin, notice: 'Comment was successfully updated.'
+    respond_to do |format|
+    if @comment.update_attributes(params[:comment].permit(:body))
+    format.html {redirect_to @pin, notice: "Comment was successfully updated"}
+    format.json {render json: @pin, status: :created, location: @comment}
     else
-    render action: 'edit'
-    end
+    format.html {render action: 'edit'}
+    format.json {render json: @comment.errors, status: :unprocessable_entity}
   end
+  end
+end
 
   def destroy
     @pin = Pin.find(params[:pin_id])
     @comment = @pin.comments.find(params[:id])
-    @comment.destroy
+    @comment.delete
     redirect_to @pin
   end
-end 
-
-    
-    
-
+end
--- db/migrate/20141218172249_create_comments.rb
+++ db/migrate/20141218172249_create_comments.rb
@@ -1,4 +1,5 @@
 class CreateComments < ActiveRecord::Migration
+	
   def change
     create_table :comments do |t|
       t.integer :pin_id
