13d1d79d5e9fd7cff7141aef64af0b156491fcc1
CODERLETICS-SHAREE <SHAREEWASHCODER@GMAIL.COM>
2015-01-07 16:23:48 -0500

    modified controller & views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,7 +14,7 @@ class CommentsController < ApplicationController
     @comment = @pin.comments.create(comment_params)
     @comment.pin_id = @pin.id
     @comment.user_id = current_user.id
-<<<<<<< HEAD
+
   if @comment.save
     flash[:success] = "Comment created!"
     redirect_to pin_path(@pin)
@@ -24,17 +24,6 @@ class CommentsController < ApplicationController
   end
 
 end
-=======
-    if @comment.save
-      flash[:success] = "Comment created!"
-      redirect_to pin_path(@pin)
-    else
-      flash[:error] = "No Comment created!"
-      redirect_to pin_path(@pin)
-    end  
-  
-  end
->>>>>>> ace6ab78c391f2e665f053ce55feeb9f8019ed2f
 
   def show
     @pin = Pin.find(params[:pin_id])
@@ -46,31 +35,18 @@ end
     @comment = @pin.comments.find(params[:id])
   end 
 
-<<<<<<< HEAD
-  
-=======
 
->>>>>>> ace6ab78c391f2e665f053ce55feeb9f8019ed2f
   def update
     @pin = Pin.find(params[:pin_id])
     @comment = @pin.comments.find(params[:id])
     if @comment.update(comment_params)
-<<<<<<< HEAD
     redirect_to @pin, notice: 'Comment was successfully updated.'
   else
     render action: 'edit'   
   end
 end
-=======
-      redirect_to @pin, notice: 'Comment was successfully updated.'
-    else
-      render action: 'edit'
-    end
-  end
 
->>>>>>> ace6ab78c391f2e665f053ce55feeb9f8019ed2f
 
-  
   def destroy
     @pin = Pin.find(params[:pin_id])
     @comment = @pin.comments.find(params[:id])
@@ -78,16 +54,8 @@ end
     redirect_to @pin
   end
 
-<<<<<<< HEAD
   private
     def comment_params 
       params.require(:comment).permit(:body)
   end
-=======
-
-  private
-    def comment_params
-      params.require(:comment).permit(:body)
-    end
->>>>>>> ace6ab78c391f2e665f053ce55feeb9f8019ed2f
 end
