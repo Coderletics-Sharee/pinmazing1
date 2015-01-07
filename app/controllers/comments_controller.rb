d9fc55a3675b39d142584d20286f55b36602998f
CODERLETICS-SHAREE <SHAREEWASHCODER@GMAIL.COM>
2015-01-07 16:06:51 -0500

    edit function works- updated with matthias corrections

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,24 +6,22 @@ class CommentsController < ApplicationController
   end
 
   def new
-    @comment = @pin.comments.build(params[:comment].permit(:body))
+    @comment = Comment.new
   end
 
   def create
     @pin = Pin.find(params[:pin_id])
-    @comment = @pin.comments.build(params[:comment].permit(:body))
+    @comment = @pin.comments.create(comment_params)
     @comment.pin_id = @pin.id
     @comment.user_id = current_user.id
-    
-    respond_to do |format|
   if @comment.save
-    format.html {redirect_to @pin, notice: "Comment was successfully created"}
-    format.json {render json: @pin, status: :created, location: @comment}
+    flash[:success] = "Comment created!"
+    redirect_to pin_path(@pin)
   else
-    format.html {render action: 'new'}
-    format.json {render json: @comment.errors, status: :unprocessable_entity}
+    flash[:error] = "No comment created!"
+    redirect_to pin_path(@pin)
   end
-  end
+
 end
 
   def show
@@ -36,26 +34,27 @@ end
     @comment = @pin.comments.find(params[:id])
   end 
 
+  
   def update
     @pin = Pin.find(params[:pin_id])
     @comment = @pin.comments.find(params[:id])
-    @pin = @comment.pin
-
-    respond_to do |format|
-    if @comment.update_attributes(params[:comment].permit(:body))
-    format.html {redirect_to @pin, notice: "Comment was successfully updated"}
-    format.json {render json: @pin, status: :created, location: @comment}
-    else
-    format.html {render action: 'edit'}
-    format.json {render json: @comment.errors, status: :unprocessable_entity}
-  end
+    if @comment.update(comment_params)
+    redirect_to @pin, notice: 'Comment was successfully updated.'
+  else
+    render action: 'edit'   
   end
 end
 
+  
   def destroy
     @pin = Pin.find(params[:pin_id])
     @comment = @pin.comments.find(params[:id])
     @comment.delete
     redirect_to @pin
+  end
+
+  private
+    def comment_params 
+      params.require(:comment).permit(:body)
   end
 end
